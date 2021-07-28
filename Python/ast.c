ffe1387685c65817871e60d80b2b6362b9feffa4
soonwoo <tnsnz@naver.com>
2021-07-28 16:52:42 +0900

    psython block_statement allow empty block. add ast.c file

--- Python/ast.c
+++ Python/ast.c
@@ -4216,6 +4216,9 @@ get_last_end_pos(asdl_seq *s, int *end_lineno, int *end_col_offset)
 {
     Py_ssize_t tot = asdl_seq_LEN(s);
     // There must be no empty suites.
+	if (tot == 0 && isCurrentFilePsython())
+		return;
+
     assert(tot > 0);
     stmt_ty last = asdl_seq_GET(s, tot - 1);
     *end_lineno = last->end_lineno;
